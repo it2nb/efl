4df89913c2c788d2b1398f2d6677d4d7e0873320
it2nb <wootichai_k@hotmail.com>
2021-12-07 10:38:19 +0700

    DB

---
+++ components/LoginForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <v-row>
+      <v-col class="text-center">
+        <h1>เข้าสู่ระบบ</h1>
+        <form @submit="Login">
+          <v-text-field
+            v-model="userName"
+            label="ชื่อผู้ใช้"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="userPassword"
+            type="password"
+            label="รหัสผ่าน"
+            required
+          ></v-text-field>
+
+          <v-btn
+            class="mr-4"
+            type="submit"
+          >
+            เข้าสู่ระบบ
+          </v-btn>
+        </form>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: '',
+      userPassword: '',
+    }
+  },
+
+  methods: {
+    Login(event) {
+      event.preventDefault()
+      console.log('User =',this.userName)
+      console.log('Pass =',this.userPassword)
+    }
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -72,7 +72,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'ระบบแฟ้มสะสมผลงานออนไลน์'
     }
   }
 }
